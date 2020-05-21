2784306f088fc524d901e25b334d31ecb58e2471
Arcadia1999 <s3721471@student.rmit.edu.au>
2020-05-21 12:03:04 +1000

    update

--- main.php
+++ main.php
@@ -1,25 +1,13 @@
 <?php
 session_start();
-putenv('TuteLab2-v1-65ee09f177f1.json');
-#
-#
-#
-#CHANGE THIS to TuteLab2-v1-65ee09f177f1.json
-#
-#
-
+putenv('My First Project-4ea46c8c82fe.json');
 
 require __DIR__ . '/vendor/autoload.php';
 
 use Google\Cloud\Datastore\DatastoreClient;
 
 # Your Google Cloud Platform project ID
-$projectId = 'tutelab2-v1';
-#
-#
-#CHANGE THIS TO CC-A2-b49a7a02359f.json
-#
-#
+$projectId = 's3721471-cc2020';
 
 # Instantiates a client
 $datastore = new DatastoreClient([
@@ -65,99 +53,37 @@ for ($i = 1; $i < 11; ++$i) {
   <meta charset="utf-8">
   <title>Main Page</title>
 
-  	
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel='stylesheet' type='text/css' href='/css/style.css'>
 </head>
-
-
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel='stylesheet' type='text/css' href='/css/style.css'>
-<!-- <style>
-html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
-</style> -->
-
-
-
 
 <body>
   <h1>ListenBrainz Analysis</h1>
 
-  <?php
-  echo "things are working yes";
-  ?>
-
-  <div name='main'>
-    <p>Songs</p>
-  </div>
-
-  <!--Div that will hold the pie chart-->
-<!-- <div class="content" >
-
-  <div id="piechart_div" style="border: 1px solid #ccc"></div>
-
-  </div> -->
-
-<div class="info">
-<p class="info">
-The website is mainly an 
-informative page of information such as popular songs 
-and popular artists over the years. It provides users 
-with data represented as graphs such as bar and pie charts 
-for clarity to easily understand the relevant information. 
-It utilises Google Cloud services such as BigQuery and Google Datastore
- to query (find) information from a public database and store it in a datastore.
-  By gathering large portions of data and condensing it into smaller pieces of
-   information to display allows quick and easy analysis for users to see.
-<br>
-
-Moving along into the technological age, walkmans, vinyl disks are slowly 
-becoming obsolete as music starts to shift towards online services where it is 
-more accessible by everyone. Similarly, music is now being sold online through websites as well as being streamed online. This website can provide users with a quick analysis of the trending bands from the past few years based off of ListenBrainz data.
-
-  </p>
-
-
-
-  <div>
-
-<!-- <div class="inlineBoxWithBorder" positio>
-  <table class="content">
+  <table class="info">
     <tr>
+      <td rowspan="2">
+        The website is mainly an
+        informative page of information such as popular songs
+        and popular artists over the years. It provides users
+        with data represented as graphs such as bar and pie charts
+        for clarity to easily understand the relevant information.
+        It utilises Google Cloud services such as BigQuery and Google Datastore
+        to query (find) information from a public database and store it in a datastore.
+        By gathering large portions of data and condensing it into smaller pieces of
+        information to display allows quick and easy analysis for users to see.
+        <br>
+        Moving along into the technological age, walkmans, vinyl disks are slowly
+        becoming obsolete as music starts to shift towards online services where it is
+        more accessible by everyone. Similarly, music is now being sold online through websites as well as being streamed online. This website can provide users with a quick analysis of the trending bands from the past few years based off of ListenBrainz data.
+      </td>
       <td>
-        <div class="content" id="piechart_div" style="border: 1px solid #ccc"></div>
+        <!--Div that will hold the pie chart-->
+        <div id="piechart_div" style="border: 1px solid #ccc"></div>
         <div id="barchart_div" style="border: 1px solid #ccc"></div>
       </td>
-    </tr>
+
   </table>
-
-  </div> -->
-
-<div class="info">
-
-  <!-- "class="inlineBoxWithBorder"  -->
-
-        <div style="display: block">
-
-
-           <div class="content" id="piechart_div"></div> 
-
-        </div> 
-
-       <div style="display: block">
-
-
-       <div id="barchart_div"></div> 
-
-
-        </div>
-
-
- </div>
-
-
 
   <!--Load the AJAX API-->
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -202,6 +128,11 @@ more accessible by everyone. Similarly, music is now being sold online through w
         'title': 'Top 10 Most Listened to Songs - ListenBrainz (2017 - 2018)',
         'width': 1000,
         'height': 650,
+        'legend': {
+          textStyle: {
+            fontSize: 14
+          }
+        }
       };
 
       // Instantiate and draw our chart, passing in some options.
@@ -229,8 +160,8 @@ more accessible by everyone. Similarly, music is now being sold online through w
       // as of 20/05/2020
       var options = {
         'title': 'Top 10 Most Listened to Artists/Bands - ListenBrainz (2017 - 2018)',
-        'width': 800,
-        'height': 500,
+        'width': 1000,
+        'height': 650,
       };
 
       // Instantiate and draw our chart, passing in some options.
